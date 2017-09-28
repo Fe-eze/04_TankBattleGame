8dc0784326a79c7964141599312509ad611a6c67
DESKTOP-0JL7JKQ\fenobus <ve>
2017-09-28 18:29:11 +0100

    Added Aiming component, delegated all aiming specific code to it. Added BlueprintCallable, subverted a crash
    NOTE: Make sure to checkout after every lecture, that crash was not funny
    SECOND NOTE: Always use Protection, unprotected null pointers are nasty!

--- TankBattle/Source/TankBattle/Private/Tank.cpp
+++ TankBattle/Source/TankBattle/Private/Tank.cpp
@@ -10,6 +10,8 @@ ATank::ATank()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// No need to protect
+	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("AimingComponent"));
 }
 
 // Called when the game starts or when spawned
@@ -35,7 +37,10 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 void ATank::AimAt(FVector HitLocation)
 {
-	auto CurrentTankName = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s Aim Direction: %s"), *CurrentTankName, *HitLocation.ToString());
+	TankAimingComponent->AimAt(HitLocation);
 }
 
+void ATank::SetBarrelReference(UStaticMeshComponent* BarrelToSet)
+{
+	TankAimingComponent->SetBarrelReference(BarrelToSet);
+}
--- TankBattle/Source/TankBattle/Public/Tank.h
+++ TankBattle/Source/TankBattle/Public/Tank.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "TankAimingComponent.h"
 #include "Tank.generated.h"
 
 UCLASS()
@@ -11,21 +12,26 @@ class TANKBATTLE_API ATank : public APawn
 {
 	GENERATED_BODY()
 
-public:
+private:
 	// Sets default values for this pawn's properties
 	ATank();
 
-protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	UTankAimingComponent* TankAimingComponent = nullptr;
+
+public:	
+
 	void AimAt(FVector HitLocation);
 	
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
 };
