7f7f39bf0de23cb9314ef8383c6cb8685c9c1515
Fe-eze Anya <fenobus@gmail.com>
2017-10-22 12:17:30 +0100

    Added Projectile class, added input binding for firing

--- TankBattle/Source/TankBattle/Public/Tank.h
+++ TankBattle/Source/TankBattle/Public/Tank.h
@@ -32,6 +32,9 @@ protected:
 public:	
 
 	void AimAt(FVector HitLocation);
+
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void Fire();
 	
 	UFUNCTION(BlueprintCallable, Category = Setup)
 	void SetBarrelReference(UTankBarrel* BarrelToSet);
