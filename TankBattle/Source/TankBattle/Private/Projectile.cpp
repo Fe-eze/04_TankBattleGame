e91686fa7bc15a73a6c17e6c6dd53b64dc3a0129
Fe-eze Anya <fenobus@gmail.com>
2017-11-03 04:36:21 +0000

    Got the tank to spit out an actual projectile. A bug appears that makes the tank not able to handle recoil, something wrong with the Physics code perhaps?

--- TankBattle/Source/TankBattle/Private/Projectile.cpp
+++ TankBattle/Source/TankBattle/Private/Projectile.cpp
@@ -2,13 +2,14 @@
 
 #include "Projectile.h"
 
-
 // Sets default values
 AProjectile::AProjectile()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ProjectileMotion = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement Comp"));
+	ProjectileMotion->bAutoActivate = false;
 }
 
 // Called when the game starts or when spawned
@@ -25,3 +26,11 @@ void AProjectile::Tick(float DeltaTime)
 
 }
 
+void AProjectile::LaunchProjectile(float Speed)
+{
+	auto Time = GetWorld()->GetTimeSeconds();
+	UE_LOG(LogTemp, Warning, TEXT("%f: Firiiiing!@!!, Speed: %f"), Time, Speed);
+
+	ProjectileMotion->SetVelocityInLocalSpace(FVector::ForwardVector * Speed);
+	ProjectileMotion->Activate();
+}
--- TankBattle/Source/TankBattle/Private/Tank.cpp
+++ TankBattle/Source/TankBattle/Private/Tank.cpp
@@ -37,17 +37,16 @@ void ATank::AimAt(FVector HitLocation)
 
 void ATank::Fire()
 {
-	auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("%f: Firiiiing!@!!"), Time);
-
 	if (!Barrel) { return; }
 
 	// Spawn projectile from front of barrel
-	GetWorld()->SpawnActor<AProjectile>(
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(
 		SetProjectileBlueprint,
 		Barrel->GetSocketLocation(FName("Projectile")),
 		Barrel->GetSocketRotation(FName("Projectile"))
 		);
+
+	Projectile->LaunchProjectile(LaunchSpeed);
 }
 
 void ATank::SetBarrelReference(UTankBarrel* BarrelToSet)
